7d6327c659cf0151feb83a9c19060027ca16cb3d
Nova <novanai.dev@gmail.com>
2022-08-12 19:25:32 +0100

    Part 4 - DataStore

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 
+import aiohttp
 import dotenv
 import hikari
 import lightbulb
@@ -16,6 +17,16 @@ bot = lightbulb.BotApp(
 )
 
 bot.load_extensions_from("./extensions/")
+
+
+@bot.listen()
+async def on_starting(event: hikari.StartingEvent) -> None:
+    bot.d.aio_session = aiohttp.ClientSession()
+
+
+@bot.listen()
+async def on_stopping(event: hikari.StoppingEvent) -> None:
+    await bot.d.aio_session.close()
 
 
 @bot.command
